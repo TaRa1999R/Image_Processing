56b35d050543bf1d1918614c38db23f7950a2ad9
TaRa1999R <rezaietara78@gmail.com>
2024-01-15 00:19:14 +0330

    Image_Processing

--- assignment26/3.py
+++ assignment26/3.py
@@ -1,9 +1,9 @@
 
 import cv2
 
-image = cv2.imread ("3_input.jpg")
+image = cv2.imread ("assignment26\inputs\input_3.jpg")
 image = cv2.rotate (image , cv2.ROTATE_180)
 
 cv2.imshow ("result 3" , image)
 cv2.waitKey ()
-cv2.imwrite ("3_output.jpg" , image)
+cv2.imwrite ("assignment26\outputs\output_3.jpg" , image)
--- assignment26/4.py
+++ assignment26/4.py
@@ -1,7 +1,7 @@
 
 import cv2
 
-img_white = cv2.imread ("4_input.jpg")
+img_white = cv2.imread ("assignment26\inputs\input_4.jpg")
 
 for row in range (120 , 220) :
     for col in range (145 , 175) :
@@ -13,4 +13,4 @@ for row in range (90 , 120) :
 
 cv2.imshow ("result 4" , img_white)
 cv2.waitKey ()
-cv2.imwrite ("4_output.jpg" , img_white)
+cv2.imwrite ("assignment26\outputs\output_4.jpg" , img_white)
