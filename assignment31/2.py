21cfcd72631b05534cab6ff8dae0e95de1ed67b6
TaRa1999R <rezaietara78@gmail.com>
2024-01-19 18:53:21 +0330

    Image_Procassing

--- assignment31/2.py
+++ assignment31/2.py
@@ -3,8 +3,17 @@ import cv2
 import numpy as np 
 
 img = cv2.imread ("inputs\input_2_rose.jpg" , cv2.IMREAD_GRAYSCALE)
+blur = cv2.GaussianBlur (img , (3 , 3) , 5)
 
+_ , thresh = cv2.threshold (img , 110 , 255 , cv2.THRESH_BINARY)
+_ , invert_thresh = cv2.threshold (img , 110 , 255 , cv2.THRESH_BINARY_INV)
 
-cv2.imshow ("" , img)
-cv2.waitKey ()
-# cv2.imwrite ("outputs\output_2_rose.jpg" , ...)
+thresh = thresh // 255
+thresh = img * thresh
+
+invert_thresh = invert_thresh // 255
+invert_thresh = img * invert_thresh
+
+invert_thresh = invert_thresh + thresh
+
+cv2.imwrite ("outputs\output_2_rose.jpg" , invert_thresh)
