695b5ee6cdc1e224091eac1e54e0fc78a3b6c3f3
TaRa1999R <rezaietara78@gmail.com>
2024-01-19 14:19:07 +0330

    Image_Processing

--- assignment31/2.py
+++ assignment31/2.py
@@ -2,4 +2,9 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("inputs\input_2_rose.jpg")
+img = cv2.imread ("inputs\input_2_rose.jpg" , cv2.IMREAD_GRAYSCALE)
+
+
+cv2.imshow ("" , img)
+cv2.waitKey ()
+# cv2.imwrite ("outputs\output_2_rose.jpg" , ...)
--- assignment31/5.py
+++ assignment31/5.py
@@ -17,35 +17,6 @@ def filtering (img , adress , n) :
     cv2.imwrite (adress , result)
 
 
-
-# def filter_5 (img , adress) :
-    # rows , cols = img.shape
-    # result = np.ones ((rows , cols) , dtype = np.uint8)
-# 
-    # filter = np.ones ((5 , 5)) / 25
-# 
-    # for i in range (2 , rows -2) :
-        # for j in range (2 , cols - 2) :
-            # small = img [i - 2 : i + 3 , j - 2 : j + 3]
-            # result = np.sum (filter * small)
-# 
-    # cv2.imwrite (adress , result)
-# 
-# 
-# def filter_15 (img , adress) :
-    # rows , cols = img.shape
-    # result = np.ones ((rows , cols) , dtype = np.uint8)
-# 
-    # filter = np.ones ((15 , 15)) / 255
-# 
-    # for i in range (7 , rows - 7) :
-        # for j in range (7 , cols - 7) :
-            # small = img [i - 7 : i + 8 , j - 7 : j + 8]
-            # result = np.sum (filter * small)
-# 
-    # cv2.imwrite (adress , result)
-
-
 img_xray = cv2.imread ("inputs\input_5_xray_noisy.png" , cv2.IMREAD_GRAYSCALE)
 img_board = cv2.imread ("inputs\input_5_board_noisy.png" , cv2.IMREAD_GRAYSCALE)
 img_noisy = cv2.imread ("inputs\input_5_image_noisy.png" , cv2.IMREAD_GRAYSCALE)
