75a468599d547dc725b5aa03d176c03dd5317038
TaRa1999R <rezaietara78@gmail.com>
2024-02-07 12:48:11 +0330

    Image_Processing

--- assignment33/5.py
+++ assignment33/5.py
@@ -2,14 +2,20 @@
 import cv2
 import numpy as np
 
+def mine_findContours (image) :
+    contours_list = ...
+
+    return contours_list
+
+
+
 img = cv2.imread ("inputs\input_BW.jpg" , cv2.IMREAD_GRAYSCALE)
 _ , thresh = cv2.threshold (img , 120 , 255 , cv2.THRESH_BINARY)
 
 # cv2 code
-
+contours , _ = cv2.findContours (thresh , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
+print (contours [0])
 
 # mine code
-
-
-cv2.imshow ("" , thresh)
-cv2.waitKey ()
+# my_contours = mine_findContours (thresh)
+# print (my_contours [0])
