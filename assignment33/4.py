39264300139086dbf951c5eb9e00dfaf36a5d672
TaRa1999R <rezaietara78@gmail.com>
2024-02-07 10:37:56 +0330

    Image_Proccessing

--- assignment33/4.py
+++ assignment33/4.py
@@ -3,17 +3,27 @@ import cv2
 import numpy as np
 
 def mine_contourArea (contour) :
-    pass
+    print (contour)
+    print (len (contour))
+    x_1 = contour[0][0][0]
+    y_1 = contour[0][0][1]
+    area = 0
+
+    return area
+
+    
 
 img = cv2.imread ("inputs\input_BW.jpg" , cv2.IMREAD_GRAYSCALE)
 _ , thresh = cv2.threshold (img , 120 , 255 , cv2.THRESH_BINARY)
 contours , _ = cv2.findContours (thresh , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
 
-for contour in contours :
+# for contour in contours :
     # cv2
-    area = cv2.contourArea (contour)
-    print (area)
+area = cv2.contourArea (contours[0])
+print (area)
 
     # mine
     # area = mine_contourArea (contour)
     # print (area)
+
+are = mine_contourArea (contours[0])
