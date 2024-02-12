03d9d102b06f1e1d39edcf3399d9a57c48dff69a
TaRa1999R <rezaietara78@gmail.com>
2024-02-12 10:05:06 +0330

    Image_Processing

--- assignment33/5.py
+++ assignment33/5.py
@@ -2,20 +2,37 @@
 import cv2
 import numpy as np
 
+def check_below () : ...
+
+
+def check_left () : ...
+
+
+def check_right () : ...
+
+
 def mine_findContours (image) :
-    contours_list = ...
-
-    return contours_list
-
-
-
+    
+    for row in range (image.shape[0]) :
+        for col in range (image.shape[1]) :
+            if image[row][col] == 255 :
+                print (f'[{col} {row}]')
+                break
+                
+    
 img = cv2.imread ("inputs\input_BW.jpg" , cv2.IMREAD_GRAYSCALE)
 _ , thresh = cv2.threshold (img , 120 , 255 , cv2.THRESH_BINARY)
+# cv2.imshow ("" , thresh)
+# cv2.waitKey ()
 
 # cv2 code
 contours , _ = cv2.findContours (thresh , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
+x,y,w,h = cv2.boundingRect (contours[0])
+cv2.rectangle (img , (x,y) , (x+w,y+h) , (255,0,0) , 3)
+cv2.imshow ("" , img)
+cv2.waitKey ()
 print (contours [0])
 
 # mine code
-# my_contours = mine_findContours (thresh)
+# my_contours = mine_findContours (thresh , )
 # print (my_contours [0])
