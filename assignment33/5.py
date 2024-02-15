f1b59b7c150812a274bc6f90bc7cd23054fd5b16
TaRa1999R <rezaietara78@gmail.com>
2024-02-15 12:24:12 +0330

    Image_Processing

--- assignment33/5.py
+++ assignment33/5.py
@@ -2,36 +2,31 @@
 import cv2
 import numpy as np
 
-def check_white (row , col) :
-    ...
-
-
 def mine_findContours (img) :
+    img_copy = np.copy (img)
+    # img_copy = 255 - img_copy
     m_contours = []
     for row in range (img.shape[0]) :
         for col in range (img.shape[1]) :
-            if img[row , col] == 255 :
+            if img [row , col] == img_copy [row , col] == 255 :
+                contour = []
+                new = [(col , row)]
+                img_copy [row , col] = 0
+                while len (new) > 0 :
+                    y , x = new.pop ()
+                    contour.append ((y , x))
+                    for dx , dy in [(0 , 1) , (0 , -1) , (1 , 0) , (-1 , 0) , (1 , 1) , (1 , -1) , (-1 , 1) , (-1 , -1)] :
+                        i = x + dx
+                        j = y + dy
+                        if 0 < i < img.shape [0] and 0 < j < img.shape [1] :
+                            if img [i , j] == img_copy [i , j] == 255 :
+                                img_copy[i , j] = 0
+                                new.append ((j , i))
+                
+                m_contours.append (np.array (contour))
+    return m_contours
 
                     
-#  contours = []
-##  visited = np.zeros_like(thresh)
-#  for i in range(thresh.shape[0]):
-   # for j in range(thresh.shape[1]):
-       # if thresh[i,j] == 255 and visited[i,j] == 0:
-           # contour = []
-           # stack = [(i,j)]
-           # visited[i,j] = 1
-           # while len(stack) > 0:
-               # x,y = stack.pop()
-               # contour.append((x,y))
-               # for dx,dy in [(0,1),(0,-1),(1,0),(-1,0),(-1,-1),(1,-1),(-1,1),(1,1)]:
-                   # nx,ny = x+dx,y+dy
-                   # if nx >= 0 and nx < thresh.shape[0] and ny >= 0 and ny < thresh.shape[1]:
-                       # if thresh[nx,ny] == 255 and visited[nx,ny] == 0:
-                           # stack.append((nx,ny))
-                           # visited[nx,ny] = 1
-           # contours.append(np.array(contour))
-#  return contours
 
 image = cv2.imread ("inputs\input_1_BW_1.jpg" , cv2.IMREAD_GRAYSCALE)
 _ , thresh = cv2.threshold (image , 170 , 255 , cv2.THRESH_BINARY)
