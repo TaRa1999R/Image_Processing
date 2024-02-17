c2c46bee0ed38d94a4004b8c769e6762f19ff75b
TaRa1999R <rezaietara78@gmail.com>
2024-02-17 21:57:01 +0330

    Image_Processing

--- assignment34/1.py
+++ assignment34/1.py
@@ -2,19 +2,25 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("inputs\input_1_friends.jpg")
+img = cv2.imread ("inputs\input_1_joey.jpeg")
 
 # cv2 code
 gray = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
+print ("gray:" , gray[0][30:45])
 
 # my code
 b , g , r = cv2.split (img)
+print ("blue:" , b[0][30:45])
+print ("green:" , g[0][30:45])
+print ("red:" , r[0][30:45])
+
 my_gray = np.zeros (img.shape , dtype = np.uint8)
 
-for i in range (img.shape [0]) :
-    for j in range (img.shape [1]) :
-        my_gray [i , j] = (b [i , j] + g [i , j] + r [i , j]) // 3
+# for i in range (img.shape [0]) :
+    # for j in range (img.shape [1]) :
+        # my_gray [i , j] = (b [i , j] + g [i , j] + r [i , j]) // 3
 
-cv2.imshow ("result 1" , my_gray)
+result = np.hstack ((gray , b , g , r ))
+cv2.imshow ("result 1" , result)
 cv2.waitKey ()
 # cv2.imwrite ("outputs\output_1friends_gray.jpg" , img)
--- assignment34/2.py
+++ assignment34/2.py
@@ -3,7 +3,46 @@ import cv2
 import numpy as np 
 
 img = cv2.imread ("inputs\input_2_rainbow.png")
+rows = 686
+cols = 1156
 
-cv2.imshow ("result 2" , img)
+r_rainbow = np.zeros ((rows , cols) , dtype = np.uint8) 
+g_rainbow = np.zeros ((rows , cols) , dtype = np.uint8) 
+b_rainbow = np.zeros ((rows , cols) , dtype = np.uint8) 
+
+# red
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (200 , 200) , 180 , 0 , 180 , 145 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (230 , 230) , 180 , 0 , 180 , 22 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (260 , 260) , 180 , 0 , 180 , 36 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (290 , 290) , 180 , 0 , 180 , 72 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (320 , 320) , 180 , 0 , 180 , 240 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (350 , 350) , 180 , 0 , 180 , 245 , 30)
+cv2.ellipse (r_rainbow , (600 , cols // 2) , (380 , 380) , 180 , 0 , 180 , 237 , 30)
+
+# green
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (200 , 200) , 180 , 0 , 180 , 39 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (230 , 230) , 180 , 0 , 180 , 67 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (260 , 260) , 180 , 0 , 180 , 242 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (290 , 290) , 180 , 0 , 180 , 163 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (320 , 320) , 180 , 0 , 180 , 236 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (350 , 350) , 180 , 0 , 180 , 148 , 30)
+cv2.ellipse (g_rainbow , (600 , cols // 2) , (380 , 380) , 180 , 0 , 180 , 63 , 30)
+
+# blue
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (200 , 200) , 180 , 0 , 180 , 240 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (230 , 230) , 180 , 0 , 180 , 227 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (260 , 260) , 180 , 0 , 180 , 242 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (290 , 290) , 180 , 0 , 180 , 74 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (320 , 320) , 180 , 0 , 180 , 14 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (350 , 350) , 180 , 0 , 180 , 13 , 30)
+cv2.ellipse (b_rainbow , (600 , cols // 2) , (380 , 380) , 180 , 0 , 180 , 5 , 30)
+
+my_rainbow = cv2.merge ([b_rainbow , g_rainbow , r_rainbow])
+for i in range (rows) :
+    for j in range (cols) :
+        if my_rainbow [i , j] == (0 , 0 , 0) :
+            my_rainbow [i , j] = (255 , 255 , 255)
+
+cv2.imshow ("result 2" , my_rainbow)
 cv2.waitKey ()
 # cv2.imwrite ("outputs\output_2_rainbow.jpg" , img)
