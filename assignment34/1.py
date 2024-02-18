70239ba6152b6e2a76a0ba555c272a18fec7f1bf
TaRa1999R <rezaietara78@gmail.com>
2024-02-19 00:45:08 +0330

    Image_Processing

--- assignment34/1.py
+++ assignment34/1.py
@@ -6,21 +6,16 @@ img = cv2.imread ("inputs\input_1_joey.jpeg")
 
 # cv2 code
 gray = cv2.cvtColor (img , cv2.COLOR_BGR2GRAY)
-print ("gray:" , gray[0][30:45])
 
 # my code
 b , g , r = cv2.split (img)
-print ("blue:" , b[0][30:45])
-print ("green:" , g[0][30:45])
-print ("red:" , r[0][30:45])
+result = np.zeros ((img.shape [0] , img.shape [1]) , dtype = np.uint8)
+for i in range (img.shape[0]) :
+    for j in range (img.shape[1]) :
+        result[i ,j] = (0.2989 * r[i , j]) + (0.5870 * g[i , j]) + (0.1140 * b[i , j])
 
-my_gray = np.zeros (img.shape , dtype = np.uint8)
-
-# for i in range (img.shape [0]) :
-    # for j in range (img.shape [1]) :
-        # my_gray [i , j] = (b [i , j] + g [i , j] + r [i , j]) // 3
-
-result = np.hstack ((gray , b , g , r ))
+cv2.imshow ("result 1" , gray)
+cv2.waitKey ()
 cv2.imshow ("result 1" , result)
 cv2.waitKey ()
-# cv2.imwrite ("outputs\output_1friends_gray.jpg" , img)
+cv2.imwrite ("outputs\output_1friends_gray.jpg" , result)
