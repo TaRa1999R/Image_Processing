62a6d3039304eb937b5a302b7f00d574d2db3e75
TaRa1999R <rezaietara78@gmail.com>
2024-02-18 11:10:54 +0330

    Image_Processing

--- assignment34/3.py
+++ assignment34/3.py
@@ -3,7 +3,17 @@ import cv2
 import numpy as np 
 
 img = cv2.imread ("inputs\input_3_watermelon.jpg")
+img = cv2.resize (img , (1000 , 700))
 
-cv2.imshow ("result 3" , img)
+b , g , r = cv2.split (img)
+
+for i in range (g.shape [0]) :
+    for j in range (g.shape [1]) :
+        if 100 < g [i , j] < 200 :
+            g [i , j] += 50
+
+result = cv2.merge ([b , r , g])
+
+cv2.imshow ("result 3" , result)
 cv2.waitKey ()
-# cv2.imwrite ("outputs\output_3_watermelon.jpg" , img)
+cv2.imwrite ("outputs\output_3_watermelon.jpg" , result)
