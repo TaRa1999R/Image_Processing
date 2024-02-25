b234097ddb3a4fdfb010238dc3168c6ae99f2827
TaRa1999R <rezaietara78@gmail.com>
2024-02-25 22:45:48 +0330

    Image_Processing

--- assignment34/5.py
+++ assignment34/5.py
@@ -1,19 +1,27 @@
 
 import cv2
 import numpy as np 
+from PIL import Image , ImageFont , ImageDraw
 
 rows = 310
 cols = 766
 
 logo = np.ones ((rows , cols , 3) , dtype = np.uint8) * 88
+logo = cv2.cvtColor (logo , cv2.COLOR_BGR2RGB)
 
 cv2.rectangle (logo , (103 , 103) , (152 , 152) , (34 , 80 , 242) , -1)
 cv2.rectangle (logo , (157 , 103) , (206 , 152) , (0 , 186 , 127) , -1)
 cv2.rectangle (logo , (103 , 157) , (152 , 206) , (239 , 164 , 0) , -1)
 cv2.rectangle (logo , (157 , 157) , (206 , 206) , (0 , 185 , 255) , -1)
 
-cv2.putText (logo , "Microsoft" , (236 , 185) , cv2.FONT_HERSHEY_TRIPLEX , 3 , (255 , 255 , 255) , 3)
+# cv2.putText (logo , "Microsoft" , (236 , 185) , cv2.FONT_HERSHEY_TRIPLEX , 3 , (255 , 255 , 255) , 3)
+
+logo_pil = Image.fromarray (logo)
+draw = ImageDraw.Draw (logo_pil)
+
+txt = "Micro"
+
 
 cv2.imshow ("result 5" , logo)
 cv2.waitKey ()
-cv2.imwrite ("outputs\output_5_microsoft_logo.jpg" , logo) 
+# cv2.imwrite ("outputs\output_5_microsoft_logo.jpg" , logo) 
