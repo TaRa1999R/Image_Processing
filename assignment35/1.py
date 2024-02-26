f3f161839cecdc9be0a2e6ff72de7011705778f4
TaRa1999R <rezaietara78@gmail.com>
2024-02-26 22:23:05 +0330

    Image_Processing

--- assignment35/1.py
+++ assignment35/1.py
@@ -3,6 +3,12 @@ import cv2
 import numpy as np 
 
 img = cv2.imread ("inputs\input_1_microsoft_logo.jpg")
+img = cv2.cvtColor (img , cv2.COLOR_BGR2BGRA)
+
+for i in range (img.shape [0]) :
+    for j in range (img.shape [1]) :
+        if img[i][j][0] == img[i][j][1] == img[i][j][2] == 88 :
+            img [i , j , 3] = 200
 
 cv2.imshow ("result 1" , img)
 cv2.waitKey ()
