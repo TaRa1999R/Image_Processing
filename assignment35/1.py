4e574621388dd5f8fae1fac8051d596201ee8d74
TaRa1999R <rezaietara78@gmail.com>
2024-02-26 20:59:42 +0330

    Image_Processing

--- assignment35/1.py
+++ assignment35/1.py
@@ -2,8 +2,8 @@
 import cv2
 import numpy as np 
 
-img = ...
+img = cv2.imread ("inputs\input_1_microsoft_logo.jpg")
 
 cv2.imshow ("result 1" , img)
 cv2.waitKey ()
-cv2.imwrite ("output\output_1_logo.png" , img)
+cv2.imwrite ("outputs\output_1_logo.png" , img)
--- assignment35/4.py
+++ assignment35/4.py
@@ -2,10 +2,8 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("inputs\input_1_microsoft_logo.jpg")
-
-# log
+img = ...
 
 cv2.imshow ("result 4" , img)
 cv2.waitKey ()
-cv2.imwrite ("outputs\output_1_logo.jpg" , img)
+cv2.imwrite ("outputs\output_4_logo.jpg" , img)
