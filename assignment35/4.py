9594651d4f228ede778eb60b2ae9036b3c330037
TaRa1999R <rezaietara78@gmail.com>
2024-02-26 20:50:44 +0330

    Image_Processing

--- assignment35/4.py
+++ assignment35/4.py
@@ -6,4 +6,4 @@ img = ...
 
 cv2.imshow ("result 4" , img)
 cv2.waitKey ()
-cv2.imwrite ("output\output_4_logo.png" , img)
+cv2.imwrite ("output\output_4_logo.jpg" , img)
---
+++ assignment35/5.py
@@ -0,0 +1,9 @@
+
+import cv2
+import numpy as np 
+
+img = ...
+
+cv2.imshow ("result 5" , img)
+cv2.waitKey ()
+cv2.imwrite ("output\output__logo.jpg" , img)
