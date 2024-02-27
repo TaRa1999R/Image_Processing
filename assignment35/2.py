18fdf8664d5b57e17760d8a181d1da5130eac3df
TaRa1999R <rezaietara78@gmail.com>
2024-02-27 20:40:27 +0330

    Image_Processing

--- assignment35/2.py
+++ assignment35/2.py
@@ -3,35 +3,40 @@ import cv2
 import numpy as np 
 
 
-cap = ..
-b , g , r = cv2.split (img)
+cap = cv2.VideoCapture (0)
+writer = cv2.VideoWriter ("outputs\output_2_color_detector.mp4" , cv2.VideoWriter_fourcc (*'mpv4') , 10 , ( 640 , 480))
 
-if b[50 , 50] <= 80 and g[50 , 50] <= 80 and r[50 , 50] <= 80 :
-    print ("black")
+while True :
+    _, frm = cap.read ()
+    # b , g , r = cv2.split (frm)
 
-elif b[50 , 50] >= 245 and g[50 , 50] >= 245 and r[50 , 50] >= 245 :
-    print ("white")
+    # if b[50 , 50] <= 80 and g[50 , 50] <= 80 and r[50 , 50] <= 80 :
+        # print ("black")
 
-elif b[50 , 50] >= 145 and g[50 , 50] <= 165 and r[50 , 50] <= 70 :
-    print ("blue")
+    # elif b[50 , 50] >= 245 and g[50 , 50] >= 245 and r[50 , 50] >= 245 :
+        # print ("white")
 
-elif b[50 , 50] <= 100 and g[50 , 50] >= 170 and r[50 , 50] <= 120 :
-    print ("green")
+    # elif b[50 , 50] >= 145 and g[50 , 50] <= 165 and r[50 , 50] <= 70 :
+        # print ("blue")
 
-elif b[50 , 50] <= 100 and 125 <= g[50 , 50] <= 200 and r[50 , 50] >= 245 :
-    print ("orange")
+    # elif b[50 , 50] <= 100 and g[50 , 50] >= 170 and r[50 , 50] <= 120 :
+        # print ("green")
 
-elif b[50 , 50] >= 120 and g[50 , 50] <= 100 and r[50 , 50] >= 90 :
-    print ("purple")
+    # elif b[50 , 50] <= 100 and 125 <= g[50 , 50] <= 200 and r[50 , 50] >= 245 :
+        # print ("orange")
 
-elif b[50 , 50] <= 100 and g[50 , 50] <= 100 and r[50 , 50] >= 160 :
-    print ("red")
+    # elif b[50 , 50] >= 120 and g[50 , 50] <= 100 and r[50 , 50] >= 90 :
+        # print ("purple")
 
-elif b[50 , 50] <= 145 and g[50 , 50] >= 225 and r[50 , 50] >= 225 :
-    print ("yellow")
+    # elif b[50 , 50] <= 100 and g[50 , 50] <= 100 and r[50 , 50] >= 160 :
+        # print ("red")
 
+    # elif b[50 , 50] <= 145 and g[50 , 50] >= 225 and r[50 , 50] >= 225 :
+        # print ("yellow")
 
+    cv2.imshow ("result 2" , frm)
+    writer.write (frm)
+    if cv2.waitKey (25) & 0xFF == ord ('q') :
+        break
 
-cv2.imshow ("result 2" , img)
-cv2.waitKey ()
-cv2.imwrite ("outputs\output_2_logo.jpg" , img)
+writer.release ()
