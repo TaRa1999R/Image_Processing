bd4f6bd9158e42af30769b0139b01c246a2e3d74
TaRa1999R <rezaietara78@gmail.com>
2024-02-27 19:14:52 +0330

    Image_Processing

--- assignment35/2.py
+++ assignment35/2.py
@@ -1,8 +1,10 @@
-
+import random
 import cv2
 import numpy as np 
 
-img = ...
+input_list = ["inputs\input_2_black.png" , "inputs\input_2_blue.png" , "inputs\input_2_green.png" , "inputs\input_2_orang.png" , "inputs\input_2_purple.png" , "inputs\input_2_red.png" , "inputs\input_2_white.png" , "inputs\input_2_yellow.png"]
+
+for i in range
 
 cv2.imshow ("result 2" , img)
 cv2.waitKey ()
