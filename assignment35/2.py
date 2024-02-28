47f0771d042fbffd2391e91d520f2b838d85f3e1
TaRa1999R <rezaietara78@gmail.com>
2024-02-28 21:49:39 +0330

    Image_Processing

--- assignment35/2.py
+++ assignment35/2.py
@@ -16,33 +16,33 @@ while True :
 
     b , g , r = cv2.split (check_color)
 
-    # if b[50 , 50] <= 80 and g[50 , 50] <= 80 and r[50 , 50] <= 80 :
-        # print ("black")
+    if b[61 , 61] <= 80 and g[61 , 61] <= 80 and r[61 , 61] <= 80 :                      #black
+        cv2.putText (blur_frm , "BLACK" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    # elif b[50 , 50] >= 245 and g[50 , 50] >= 245 and r[50 , 50] >= 245 :
-        # print ("white")
+    elif b[61 , 61] >= 200 and g[61 , 61] >= 200 and r[61 , 61] >= 200 :                 #white
+        cv2.putText (blur_frm , "WHITE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 ,10))
 
-    # elif b[50 , 50] >= 145 and g[50 , 50] <= 165 and r[50 , 50] <= 70 :
-        # print ("blue")
+    elif b[61 , 61] >= 145 and g[61 , 61] <= 165 and r[61 , 61] <= 70 :                  #blue
+        cv2.putText (blur_frm , "BLUE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 ,10))
 
-    # elif b[50 , 50] <= 100 and g[50 , 50] >= 170 and r[50 , 50] <= 120 :
-        # print ("green")
+    elif b[61 , 61] >= 50 and g[61 , 61] >= 70 and r[61 , 61] <= 45 :                 #green
+        cv2.putText (blur_frm , "GREEN" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    # elif b[50 , 50] <= 100 and 125 <= g[50 , 50] <= 200 and r[50 , 50] >= 245 :
-        # print ("orange")
+    elif b[61 , 61] <= 81 and 88 <= g[61 , 61] <= 165 and r[61 , 61] >= 160 :         #orange
+        cv2.putText (blur_frm , "ORANGE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 , 10))
 
-    # elif b[50 , 50] >= 120 and g[50 , 50] <= 100 and r[50 , 50] >= 90 :
-        # print ("purple")
+    elif b[61 , 61] >= 120 and g[61 , 61] <= 100 and r[61 , 61] >= 90 :                 #purple
+        cv2.putText (blur_frm , "PURPLE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 , 10))
 
-    # elif b[50 , 50] <= 100 and g[50 , 50] <= 100 and r[50 , 50] >= 160 :
-        # print ("red")
+    elif b[61 , 61] <= 100 and g[61 , 61] <= 100 and r[61 , 61] >= 160 :                 #red
+        cv2.putText (blur_frm , "RED" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 , 10))
 
-    # elif b[50 , 50] <= 145 and g[50 , 50] >= 225 and r[50 , 50] >= 225 :
-        # print ("yellow")
+    elif b[61 , 61] <= 100 and g[61 , 61] >= 200 and r[61 , 61] >= 200 :                 #yellow
+        cv2.putText (blur_frm , "YELLOW" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (10 , 10 , 10))
 
     cv2.imshow ("result 2" , blur_frm)
-    # writer.write (frm)
+    writer.write (frm)
     if cv2.waitKey (25) & 0xFF == ord ('q') :
         break
 
-# writer.release ()
+writer.release ()
