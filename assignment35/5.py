8f5d1be8631ebb01ddcc40485e4a90f86630957b
TaRa1999R <rezaietara78@gmail.com>
2024-03-01 00:22:42 +0330

    Image_Processing

--- assignment35/5.py
+++ assignment35/5.py
@@ -2,14 +2,21 @@
 import cv2
 import numpy as np 
 
-def encrypt_img (img , key) :
+def encrypt (img , key) :
     img_np = np.array (img)
+    key_np = np.array (key)
+
+    result = np.bitwise_xor (img_np , key_np)
+    return result
 
 image = cv2.imread ("inputs\input_5_Mona_Lisa.jpg")
+key = np.random.randint (0 , 256 , image.shape , np.uint8)
 
-cv2.imshow ("result 5" , ...)
+encrypt_img = encrypt (image , key)
+
+cv2.imshow ("result 5 : Encrypt Image" , encrypt_img)
 cv2.waitKey ()
-# cv2.imwrite ("outputs\output_5_encryption.jpg" , ...)
+cv2.imwrite ("outputs\output_5_encryption.jpg" , encrypt_img)
 
 # cv2.imshow ("result 5" , ...)
 # cv2.waitKey ()
