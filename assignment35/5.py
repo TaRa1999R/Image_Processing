246dcdbebb233022e9986f46bf5ff807d81315d0
TaRa1999R <rezaietara78@gmail.com>
2024-03-01 00:17:16 +0330

    Image_Processing

--- assignment35/5.py
+++ assignment35/5.py
@@ -2,12 +2,15 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("inputs\input_5_Mona_Lisa.jpg")
+def encrypt_img (img , key) :
+    img_np = np.array (img)
+
+image = cv2.imread ("inputs\input_5_Mona_Lisa.jpg")
 
 cv2.imshow ("result 5" , ...)
 cv2.waitKey ()
-cv2.imwrite ("outputs\output_5_encryption.jpg" , ...)
+# cv2.imwrite ("outputs\output_5_encryption.jpg" , ...)
 
-cv2.imshow ("result 5" , ...)
-cv2.waitKey ()
-cv2.imwrite ("outputs\output_5_dencryption.jpg" , ...)
+# cv2.imshow ("result 5" , ...)
+# cv2.waitKey ()
+# cv2.imwrite ("outputs\output_5_dencryption.jpg" , ...)
