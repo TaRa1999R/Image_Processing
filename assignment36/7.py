4d83f871f38f566041ad3d96df48743e60c733f2
TaRa1999R <rezaietara78@gmail.com>
2024-03-06 22:54:58 +0330

    Image_Processing

--- assignment36/7.py
+++ assignment36/7.py
@@ -5,7 +5,19 @@ import numpy as np
 img = cv2.imread ("inputs\input_7_spiderman.jpeg")
 img = cv2.resize (img , (500 , 750))
 
+img_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
+img_hsv = cv2.cvtColor (img_rgb , cv2.COLOR_RGB2HSV)
 
-cv2.imshow ("result 7" , img)
+h , s , v = cv2.split (img_hsv)
+
+for i in range (h.shape [0]) :
+    for j in range (h.shape [1]) :
+        if h [i , j] < 15 or h [i , j] > 165 :
+          h [i , j] += 60
+
+result = cv2.merge ((h , s , v))
+result = cv2.cvtColor (result , cv2.COLOR_HLS2RGB)
+
+cv2.imshow ("result 7" , result)
 cv2.waitKey ()
 # cv2.imwrite ("outputs\output_7_spiderman_new_outfit.jpg" , img)
