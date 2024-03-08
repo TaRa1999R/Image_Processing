52260b6e2af0a7fd5aaeb56bd89a4fc1a49ea78a
TaRa1999R <rezaietara78@gmail.com>
2024-03-09 00:04:45 +0330

    Image_Processing

--- assignment36/7.py
+++ assignment36/7.py
@@ -14,14 +14,12 @@ for i in range (h.shape [0]) :
     for j in range (h.shape [1]) :
         if h [i , j] < 15 or h [i , j] > 165 :
           if s [i , j] > 150 :
-            h [i , j] += 60
+            h [i , j] += 50 
 
 
 result = cv2.merge ((h , s , v))
-result = cv2.cvtColor (result , cv2.COLOR_HSV2RGB)
+result = cv2.cvtColor (result , cv2.COLOR_HSV2BGR)
 
-cv2.imshow ("result 7" , img)
-cv2.waitKey ()
 cv2.imshow ("result 7" , result)
 cv2.waitKey ()
-cv2.imwrite ("outputs\output_7_spiderman_new_outfit.jpg" , result)
+# cv2.imwrite ("outputs\output_7_spiderman_new_outfit.jpg" , result)
