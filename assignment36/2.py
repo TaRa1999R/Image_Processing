bf8669a7e7242ef203c1d28dd86c9973ba687f64
TaRa1999R <rezaietara78@gmail.com>
2024-03-09 20:26:41 +0330

    Image_Processing

--- assignment36/2.py
+++ assignment36/2.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np 
 
 cap = cv2.VideoCapture (0)
-# writer = cv2.VideoWriter ("outputs\output_2_color_detector.mp4" , cv2.VideoWriter_fourcc (*'mpv4') , 10 , (640 , 480))
+writer = cv2.VideoWriter ("outputs\output_2_color_detector.mp4" , cv2.VideoWriter_fourcc (*'mpv4') , 10 , (640 , 480))
 
 while True :
     _ , frm = cap.read ()
@@ -12,34 +12,34 @@ while True :
 
     check_color = frm [179:302 , 259:382]
     blur_frm = cv2.blur (frm , (200 , 200))
-    
+
     rgb_color = cv2.cvtColor (check_color , cv2.COLOR_BGR2RGB)
     hsv_color = cv2.cvtColor (rgb_color , cv2.COLOR_RGB2HSV)
 
     h , s , v = cv2.split (hsv_color)
     
-    if ... :                      #black
+    if v [61 , 61] < 20 :                      #black
         cv2.putText (blur_frm , "BLACK" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #white
+    elif s [61 , 61] < 20 :                      #white
         cv2.putText (blur_frm , "WHITE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ...:                      #red
+    elif h [61 , 61] < 15 or h [61 , 61] > 165 :                      #red
         cv2.putText (blur_frm , "RED" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #purple
+    elif 140 < h [61 , 61] < 165 :                      #purple
         cv2.putText (blur_frm , "PURPLE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #blue
+    elif 105 < h [61 , 61] < 140 :                      #blue
         cv2.putText (blur_frm , "BLUE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #green
+    elif 50 < h [61 , 61] < 105 :                      #green
         cv2.putText (blur_frm , "GREEN" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #yellow
+    elif 22 < h [61 , 61] < 50 :                      #yellow
         cv2.putText (blur_frm , "YELLOW" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
-    elif ... :                      #orange
+    elif 15 < h [61 , 61] < 22 :                      #orange
         cv2.putText (blur_frm , "ORANGE" , (180 , 150) , cv2.FONT_HERSHEY_COMPLEX , 3 , (200 , 200 , 200))
 
     else :
@@ -50,8 +50,8 @@ while True :
     blur_frm [179:302 , 259:382] = check_color
 
     cv2.imshow ("result 2" , blur_frm)
-    # writer.write (blur_frm)
+    writer.write (blur_frm)
     if cv2.waitKey (25) & 0xFF == ord ('q') :
         break
 
-# writer.release ()
+writer.release ()
