a63441e1659ce3741d8a8fa86e4f3998bd39b448
TaRa1999R <rezaietara78@gmail.com>
2024-03-13 00:31:00 +0330

    Image_Processing

--- assignment36/4.py
+++ assignment36/4.py
@@ -2,8 +2,8 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("")
-
+img = cv2.imread ("outputs\output_1_materwelon.jpg")
+img = cv2.resize (img , (326 , 217))
 cv2.imshow ("result 4" , img)
 cv2.waitKey ()
-# cv2.imwrite ("outputs\output_4_skin_detection.jpg" , img)
+cv2.imwrite ("outputs\output_1_materwelon2.jpg" , img)
