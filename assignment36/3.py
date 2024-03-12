6c22c70ec8141ac11d21da01f3f6ef740c028c95
TaRa1999R <rezaietara78@gmail.com>
2024-03-12 20:29:25 +0330

    Image_Processing

--- assignment36/3.py
+++ assignment36/3.py
@@ -11,7 +11,9 @@ img_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
 img_hsv = cv2.cvtColor (img_rgb , cv2.COLOR_RGB2HSV)
 
 mask = cv2.inRange (img_hsv , lower_h , upper_h)
-
+mask = (mask / 255).astype (int)
+result = cv2.merge ([img[: , : , 0] * mask , img[: , : , 1] * mask , img[: , : , 2] * mask])
+result = result.astype(np.uint8)
 
 cv2.imshow ("result 3" , result)
 cv2.waitKey ()
