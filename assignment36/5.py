8ee35eda950ce56a93f8a737a70c100cdcb55825
TaRa1999R <rezaietara78@gmail.com>
2024-03-12 22:55:23 +0330

    Image_Processing

--- assignment36/5.py
+++ assignment36/5.py
@@ -5,8 +5,15 @@ import numpy as np
 img = cv2.imread ("inputs\input_5_superman.jpg")
 img = cv2.resize (img , (1100 , 750))
 
-back = cv2.imread ("inputs\input_5_milad_day.jpg")
-back = cv2.resize (back , (1100 , 750))
+background = cv2.imread ("inputs\input_5_milad_day.jpg")
+background = cv2.resize (background , (1100 , 750))
+
+s_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
+s_hsv = cv2.cvtColor (s_rgb , cv2.COLOR_RGB2HSV)
+
+bg_rgb = cv2.cvtColor (background , cv2.COLOR_BGR2RGB)
+bg_hsv = cv2.cvtColor (bg_rgb , cv2.COLOR_RGB2HSV)
+
 
 
 cv2.imshow ("result 5" , back)
