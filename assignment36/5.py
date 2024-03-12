f6a1881fe2a86b1d455d197cc2337a3ca83112e3
TaRa1999R <rezaietara78@gmail.com>
2024-03-12 23:47:27 +0330

    Image_Processing

--- assignment36/5.py
+++ assignment36/5.py
@@ -5,21 +5,18 @@ import numpy as np
 img = cv2.imread ("inputs\input_5_superman.jpg")
 img = cv2.resize (img , (1100 , 750))
 
-background = cv2.imread ("inputs\input_5_milad_day.jpg")
+background = cv2.imread ("inputs\input_5_teh.jpg")
 background = cv2.resize (background , (1100 , 750))
 
 s_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
 s_hsv = cv2.cvtColor (s_rgb , cv2.COLOR_RGB2HSV)
-h , _ , _ = cv2.split (s_hsv)
-
-bg_rgb = cv2.cvtColor (background , cv2.COLOR_BGR2RGB)
-bg_hsv = cv2.cvtColor (bg_rgb , cv2.COLOR_RGB2HSV)
+h , _ , v = cv2.split (s_hsv)
 
 for i in range (img.shape[0]) :
     for j in range (img.shape[1]) :
-        if 31 < h [i , j] < 83 :
+        if 35 < h [i , j] < 74 and v [i , j] > 85 :
             img [i , j] = background [i , j]
 
 cv2.imshow ("result 5" , img)
 cv2.waitKey ()
-# cv2.imwrite ("outputs\output_5_superman.jpg" , img)
+cv2.imwrite ("outputs\output_5_superman.jpg" , img)
