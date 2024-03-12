0ab50dc106b7a7354d6d3d23616ee6e6934246d9
TaRa1999R <rezaietara78@gmail.com>
2024-03-12 19:54:07 +0330

    Image_Processing

--- assignment36/3.py
+++ assignment36/3.py
@@ -2,8 +2,17 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("")
+img = cv2.imread ("inputs\input_3_balloons.jpg")
 
-cv2.imshow ("result 3" , img)
+lower_h = np.array ([160 , 0 , 0])
+upper_h = np.array ([180 , 255 , 255])
+
+img_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
+img_hsv = cv2.cvtColor (img_rgb , cv2.COLOR_RGB2HSV)
+
+mask = cv2.inRange (img_hsv , lower_h , upper_h)
+
+
+cv2.imshow ("result 3" , mask)
 cv2.waitKey ()
 # cv2.imwrite ("outputs\output_3_Balloon_detection.jpg" , img)
