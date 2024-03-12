71b53525728840701dad94778719cdf233dcffcc
TaRa1999R <rezaietara78@gmail.com>
2024-03-12 23:05:28 +0330

    Image_Processing

--- assignment36/5.py
+++ assignment36/5.py
@@ -10,12 +10,16 @@ background = cv2.resize (background , (1100 , 750))
 
 s_rgb = cv2.cvtColor (img , cv2.COLOR_BGR2RGB)
 s_hsv = cv2.cvtColor (s_rgb , cv2.COLOR_RGB2HSV)
+h , _ , _ = cv2.split (s_hsv)
 
 bg_rgb = cv2.cvtColor (background , cv2.COLOR_BGR2RGB)
 bg_hsv = cv2.cvtColor (bg_rgb , cv2.COLOR_RGB2HSV)
 
+for i in range (img.shape[0]) :
+    for j in range (img.shape[1]) :
+        if 31 < h [i , j] < 83 :
+            img [i , j] = background [i , j]
 
-
-cv2.imshow ("result 5" , back)
+cv2.imshow ("result 5" , img)
 cv2.waitKey ()
 # cv2.imwrite ("outputs\output_5_superman.jpg" , img)
