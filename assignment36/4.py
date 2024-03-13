647bc51701080817e011bb8a71d7bbba4573cd89
TaRa1999R <rezaietara78@gmail.com>
2024-03-14 00:02:58 +0330

    Image_Processing

--- assignment36/4.py
+++ assignment36/4.py
@@ -2,8 +2,15 @@
 import cv2
 import numpy as np 
 
-img = cv2.imread ("inputs\input_6_tara.jpg")
-img = cv2.resize (img , (370 , 750))
-cv2.imshow ("" , img)
-cv2.waitKey ()
-cv2.imwrite ("inputs\input_6_tara_resized.jpg" , img)
+cap = cv2.VideoCapture (0)
+writer = cv2.VideoWriter ("outputs\output_4_skin_detector.mp4" , cv2.VideoWriter_fourcc (*'mpv4') , 10 , (640 , 480))
+
+while True :
+    _ , frm = cap.read ()
+
+    cv2.imshow ("result 4" , frm)
+    writer.write (frm)
+    if cv2.waitKey (25) & 0xFF == ord ('q') :
+        break
+
+writer.release ()
